5882bf06355d1bc54ca6f76f686f783c99be3381
Katya Arutyunova <91216806+arutyunv@users.noreply.github.com>
2026-04-15 11:51:02 +0300

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,17 @@
+### More Interesting Approach w/ Hash Set ###
+
+# Instead of checking each element manually, we simply compare the length of the set to the length of the original array.
+# If duplicates exist, the set will contain fewer elements.
+
+# Same idea as before, but shorter and more concise implementation of it
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        # Convert the array into a hash set, which removes duplicates.
+        # Converting to set removed duplicates 
+        nums_set = set(nums)
+
+        # Compare the length of nums set and length of the array 
+        return len(nums_set) < len(nums)
+        
